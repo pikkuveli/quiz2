31f95ed7a549e72e90d6705b37497de7d43b5e26
pikkuveli <51536585+pikkuveli@users.noreply.github.com>
2019-06-14 20:28:02 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -76,28 +76,20 @@ const quizdata=[
         category:1
       }
     ];
+
 /** Random shuffle questions **/
 function shuffleArray(question){
-   var shuffled=question.sort(function() {
-    return .5 - Math.random();
- });
-   return shuffled;
-}
-
-function shuffle(a) {
-  for (var i = a.length; i; i--) {
-    var j = Math.floor(Math.random() * i);
-    var _ref = [a[j], a[i - 1]];
-    a[i - 1] = _ref[0];
-    a[j] = _ref[1];
-  }
-}
-
-/*** Return shuffled question ***/
-function generateQuestions(){
-  var questions=shuffleArray(quizdata);    
-  return questions;
-}
+    var shuffled=question.sort(function() {
+     return .5 - Math.random();
+  });
+    return shuffled;
+ }
+ 
+ /*** Return shuffled question ***/
+ function generateQuestions(){
+   var questions=shuffleArray(quizdata);    
+   return questions;
+ }
 
 /*** Return list of options ***/
 function returnOptionList(opts, i){
@@ -158,28 +150,13 @@ function correctAnswerArray(resultByCat){
 
   return arrayForChart;
 }
+
 /** Generate array for percentage calculation **/
 function genResultArray(results, wrong){
   var resultByCat = resultByCategory(results);
   var arrayForChart=correctAnswerArray(resultByCat);
   arrayForChart.push(wrong);
-  return arrayForChart
-}
-
-/** percentage Calculation **/
-function percentCalculation(array, total){
-  var percent = array.map(function (d, i) {
-    return (100 * d / total).toFixed(2);
-  });
-  return percent;
-}
-
-/*** Get percentage for chart **/
-function getPercentage(resultByCat, wrong){
-  var totalNumber=resultList.length;
-  var wrongAnwer=wrong;
-  //var arrayForChart=genResultArray(resultByCat, wrong);
-  //return percentCalculation(arrayForChart, totalNumber);
+  return arrayForChart;
 }
 
 /** count right and wrong answer number **/
@@ -220,80 +197,31 @@ function resultByCategory(results){
   return categoryCount;
 }
 
-
-/** Total score pie chart**/
-function totalPieChart(_upto, _cir_progress_id, _correct, _incorrect) {
-
-   $("#"+_cir_progress_id).find("._text_incor").html("Väärin: "+_incorrect);
-   $("#"+_cir_progress_id).find("._text_cor").html("Oikein: "+_correct);
-
-   var unchnagedPer=_upto;
-   
-    _upto = (_upto > 100) ? 100 : ((_upto < 0) ? 0 : _upto);
-
-    var _progress = 0;
-
-    var _cir_progress = $("#"+_cir_progress_id).find("._cir_P_y");
-    var _text_percentage = $("#"+_cir_progress_id).find("._cir_Per");
-
-    var _input_percentage;
-    var _percentage;
-
-    var _sleep = setInterval(_animateCircle, 25);
-
-    function _animateCircle() {
-      //2*pi*r == 753.6 +xxx=764
-        _input_percentage = (_upto / 100) * 764;
-        _percentage = (_progress / 100) * 764;
-
-        _text_percentage.html(_progress + '%');
-
-        if (_percentage >= _input_percentage) {
-             _text_percentage.html('<tspan x="50%" dy="0em">'+unchnagedPer + '% </tspan><tspan  x="50%" dy="1.9em">Tuloksesi</tspan>');
-            clearInterval(_sleep);
-        } else {
-
-            _progress++;
-
-            _cir_progress.attr("stroke-dasharray",_percentage + ',764');
-        }
-    }
-}
-
-function renderBriefChart(correct, total, incorrect){
-  var percent=(100 * correct / total);
-  if(Math.round(percent) !== percent) {
-          percent = percent.toFixed(2);
-  }
-
- totalPieChart(percent, '_cir_progress', correct, incorrect)
-   
-}
-
 /** List question and your answer and correct answer  
 
 *****/
 function getAllAnswer(results){
-    var innerhtml="";
-    for(var i=0;i<results.length;i++){
+  var innerhtml="";
+  for(var i=0;i<results.length;i++){
 
-      var _class=((results[i].iscorrect)?"item-correct":"item-incorrect");
-       var _classH=((results[i].iscorrect)?"h-correct":"h-incorrect");
-      
+    var _class=((results[i].iscorrect)?"item-correct":"item-incorrect");
+     var _classH=((results[i].iscorrect)?"h-correct":"h-incorrect");
+    
 
-      var _html='<div class="_resultboard '+_class+'">'+
-                  '<div class="_header">'+results[i].question+'</div>'+
-                  '<div class="_yourans '+_classH+'">'+results[i].clicked+'</div>';
+    var _html='<div class="_resultboard '+_class+'">'+
+                '<div class="_header">'+results[i].question+'</div>'+
+                '<div class="_yourans '+_classH+'">'+results[i].clicked+'</div>';
 
-        var html="";
-       if(!results[i].iscorrect)
-        html='<div class="_correct">'+results[i].answer+'</div>';
-       _html=(_html+html)+'</div>';
-       innerhtml+=_html;
-    }
+      var html="";
+     if(!results[i].iscorrect)
+      html='<div class="_correct">'+results[i].answer+'</div>';
+     _html=(_html+html)+'</div>';
+     innerhtml+=_html;
+  }
 
-  $(".allAnswerBox").html('').append(innerhtml);
+$(".allAnswerBox").html('').append(innerhtml);
 }
+
 /** render  Brief Result **/
 function renderResult(resultList){ 
   
@@ -306,14 +234,6 @@ function renderResult(resultList){
   renderBriefChart(countCorrect, resultList.length, countWrong);
 }
 
-function renderChartResult(){
-   var results=resultList; 
-  var countCorrect=countAnswers(results)[0], 
-  countWrong=countAnswers(results)[1];
-  var dataForChart=genResultArray(resultList, countWrong);
-  renderChart(dataForChart);
-}
-
 /** Insert progress bar in html **/
 function getProgressindicator(length){
   var progressbarhtml=" ";
@@ -321,7 +241,7 @@ function getProgressindicator(length){
     progressbarhtml+='<div class="my-progress-indicator progress_'+(i+1)+' '+((i==0)?"active":"")+'"></div>';
   }
   $(progressbarhtml).insertAfter(".my-progress-bar");
-} 
+}
 
 /*** change progress bar when next button is clicked ***/
 function changeProgressValue(){
@@ -337,8 +257,8 @@ function changeProgressValue(){
     $('progress').val($progressValue);
   }
   $('.js-my-progress-completion').html($('progress').val() + '% complete');
+}
 
-}   
 function addClickedAnswerToResult(questions,presentIndex,clicked ){
   var correct=getCorrectAnswer(questions, presentIndex);
     var result={
@@ -352,8 +272,7 @@ function addClickedAnswerToResult(questions,presentIndex,clicked ){
     resultList.push(result);
 
     console.log("result");
-    console.log(result);
-      
+    console.log(result);     
 }
 
 $(document).ready(function() {
@@ -402,26 +321,14 @@ $(document).ready(function() {
 
   });
 
-  
-  
-
-   $(".resultArea").on('click','.viewchart',function(){
-      $(".resultPage2").show();
-       $(".resultPage1").hide();
-       $(".resultPage3").hide();
-       renderChartResult();
-   });
-
     $(".resultArea").on('click','.backBtn',function(){
       $(".resultPage1").show();
-       $(".resultPage2").hide();
        $(".resultPage3").hide();
         renderResult(resultList);
    });
 
     $(".resultArea").on('click','.viewanswer',function(){
       $(".resultPage3").show();
-       $(".resultPage2").hide();
        $(".resultPage1").hide();
         getAllAnswer(resultList);
    });
@@ -431,3 +338,4 @@ $(document).ready(function() {
   });
 
 });
+
